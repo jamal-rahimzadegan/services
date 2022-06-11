4756e4317e2c2f69ea5a98b1f0dbeb44a42538f8
Jamal <jamalrahimzadegan@gmail.com>
2022-06-11 10:59:56 +0430

    Delete src/web-worker-service directory

--- src/web-worker-service/test-worker.js
+++
@@ -1,9 +0,0 @@
-function testWorker() {
-  this.onmessage = (e) => {
-    for (let i = 0; i < e.data; i++) postMessage(`from worker: ${i + 1}`);
-  };
-}
-
-testWorker();
-
-module.exports = testWorker;
